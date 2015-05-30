6978577fdf63f3f02ca9c3ac842abab583811f93
Gabe Rosenhouse <rosenhouse@gmail.com>
2015-05-30 11:38:10 -0700

    Consolidate creation of AES-GCM cipher mode into a single function

--- symmetric.go
+++ symmetric.go
@@ -15,31 +15,29 @@ const (
 // http://csrc.nist.gov/publications/nistpubs/800-38D/SP-800-38D.pdf
 var zeroNonce []byte = make([]byte, symmetricNonceLength)
 
-func symmetricDecrypt(aesKey, ciphertext []byte) ([]byte, error) {
+func makeAESGCM(aesKey []byte) (cipher.AEAD, error) {
 	blockCipher, err := aes.NewCipher(aesKey)
 	if err != nil {
 		return nil, err
 	}
 
-	streamCipher, err := cipher.NewGCM(blockCipher)
+	return cipher.NewGCM(blockCipher)
+}
+
+func symmetricDecrypt(aesKey, ciphertext []byte) ([]byte, error) {
+	gcm, err := makeAESGCM(aesKey)
 	if err != nil {
 		return nil, err
 	}
 
-	return streamCipher.Open(nil, zeroNonce, ciphertext, nil)
+	return gcm.Open(nil, zeroNonce, ciphertext, nil)
 }
 
 func symmetricEncrypt(aesKey, plaintext []byte) ([]byte, error) {
-	blockCipher, err := aes.NewCipher(aesKey)
+	gcm, err := makeAESGCM(aesKey)
 	if err != nil {
-		return []byte{}, err
+		return nil, err
 	}
 
-	streamCipher, err := cipher.NewGCM(blockCipher)
-	if err != nil {
-		return []byte{}, err
-	}
-
-	ciphertext := streamCipher.Seal(nil, zeroNonce, plaintext, nil)
-	return ciphertext, nil
+	return gcm.Seal(nil, zeroNonce, plaintext, nil), nil
 }
